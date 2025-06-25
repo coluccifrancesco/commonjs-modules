e825baae60a987a2b5e056499fc4adb6be7a1897
coluccifrancesco <coluccif2002@gmail.com>
2025-06-25 16:19:54 +0200

    Add people module to combine full name and hobbies; implement logging for multiple entries

---
+++ bonus/people.js
@@ -0,0 +1,15 @@
+const fullName = require('../names');
+const hobbies = require('../hobbies')
+
+function nameNHobbies (x, y){
+
+    return {
+        fullName: x,
+        hobbies: y
+    }
+}
+
+console.log(nameNHobbies(fullName('Francesco', 'Colucci'), hobbies('Mtb', 'Watches', 'Cars')));
+console.log(nameNHobbies(fullName('Paolo', 'Duzioni'), hobbies('No-bug code', 'Stardew Valley', 'Hungary')));
+console.log(nameNHobbies(fullName('Artur', 'Mamedov'), hobbies('Freud', 'Clean Coding', 'Backend')));
+console.log(nameNHobbies(fullName('Fabio ', 'Pacifici'), hobbies('Php', 'Cats', 'Star Wars')));
