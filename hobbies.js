b186a1f1bc0cff2284819b53be6231e7b1be73fb
coluccifrancesco <coluccif2002@gmail.com>
2025-06-25 15:31:58 +0200

    Add hobbies and names modules; update .gitignore to exclude .Ds-Store

---
+++ hobbies.js
@@ -0,0 +1,9 @@
+function hobbies (hobbyOne, hobbyTwo, hobbyThree){
+    const hobbiesList = [];
+    hobbiesList.push(hobbyOne, hobbyTwo, hobbyThree);
+    console.log(hobbiesList);
+    
+    return hobbiesList
+}
+
+module.exports = hobbies;
---
+++ names.js
@@ -0,0 +1,7 @@
+function name (firstName, lastName){
+    const personName = firstName + ' ' +lastName;
+    console.log(personName);
+    return personName
+}
+
+module.exports = name;
